5bcb86b87da375f45cd0aa7498d02a23ce7a202f
Marek Matczak <mmatczak@autograf.pl>
2020-12-08 09:00:22 +0100

    Arrays

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-// data goes down
-// events go up
-// class Employee {
-//     constructor(firstName, lastName) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//     }
-// }
+const myNumbers = [1, 3, 5, 6, 8];
+const names = ['Marek', 'John', 'James'];
+const person = [{
+    name: 'Marek',
+    age: 41
+}];
+
+// myNumbers.push(2, 4, 4);
+// myNumbers.push();
+// myNumbers.splice()
+
+let sum = 0;
+for (let i = 0; i < myNumbers.length; i++) {
+    const myNumber = myNumbers[i];
+    const biggerNumber = add2(myNumber)
+    if (onlyGreaterThan5(biggerNumber)) {
+        sum = sum + biggerNumber;
+    }
+}
+
+console.log(sum);
+
+console.log(myNumbers
+    .map(add2)
+    .filter(onlyGreaterThan5)
+    .reduce((sum, myNumber) => sum + myNumber, 0)
+);
+
+function onlyGreaterThan5(myNumber) {
+    return myNumber > 5;
+}
+
+function add2(myNumber) {
+    return myNumber + 2;
+}
 
 
 
-const formElement = document.querySelector('form');
-formElement.addEventListener('submit', function handleSubmit (event) {
-    event.preventDefault();
 
-    const form = event.target;
-    const firstNameElement = form.querySelector('#firstName');
-    const firstName = firstNameElement.value;
-    const lastNameElement = form.querySelector('#lastName');
-    const lastName = lastNameElement.value;
-
-    console.log({
-        firstName, lastName
-    });
-})
